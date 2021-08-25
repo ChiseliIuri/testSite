233a7f04c36576925e8865690082b64f237eb1e1
i.chiseli <test@mail.com>
2021-08-25 17:34:33 +0300

    big comm

--- index.php
+++ index.php
@@ -3,122 +3,73 @@
 spl_autoload_register(function ($class) {
     $b = '.' . DIRECTORY_SEPARATOR;
     $classes_dir = [$b . 'models', $b . 'controllers'];
-    foreach($classes_dir as $dir) {
+    foreach ($classes_dir as $dir) {
         $path = $dir . DIRECTORY_SEPARATOR . $class . '.php';
-        if(is_file($path)) {
+        if (is_file($path)) {
             require_once($path);
             break;
         }
     }
 });
+function view($view, $ar){
+    ob_start();
+    extract($ar);
+    require_once $view;
+    return ob_get_clean();
+}
+DB::connect("127.0.0.1", "root", "", "test");
 
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tables</title>
-    <link rel="stylesheet" type="text/css" href="assets/style.css">
-    <!--    <link href="assets/css/a" rel="stylesheet">-->
-</head>
-
-<body class="fon">
-
-<div class="sticky">
-    <ul class="nav">
-        <li><a href="inddex.html">Home</a></li>
-        <li><a href="#">Setings</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">New</a></li>
-    </ul>
-</div>
-
-<div id="contentPos">
-
-    <h1 class="center" title="About wtf is this.">CSS begin</h1>
-    <div id="link">
-        <a href="https://www.go.com/">Apasa aici pentru a cunoaste lumea durerii.</a><br/>
-        <a href="https://www.facebook.com">Apasa aici pentru a cunoaste lumea placerii.</a>
-    </div>
-
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
-        in culpa qui officia deserunt mollit anim id est laborum.
-    </p> <br/>
-
-    <div class="responsive">
-        <div class="boxForm">
-            <span style="background-color: #3c763d; padding: 6px; font-size: 20px; margin-bottom: 20px; border-radius: 5px; cursor: pointer"> Hover here for visualize \/ </span><br/>
-
-            <form action="controllers/IndexController.php" method="post" class="form">
-                <div style="color: black; font-size: 2em">
-                    Introduceti un User in db:
-                </div>
-                Numele: <br/> <input type="text" name="fName"><br/>
-                Prenumele:<br/> <input type="text" name="lName"><br/>
-                Varsta:<br/> <input type="number" name="age"><br/>
-                <input title="asdf" type="submit">
-            </form>
-        </div>
-
-    </div>
-    <div class="responsive">
-        <div id="tableResp">
-            <table>
-                <th colspan="5" style="text-align: center">Users</th>
-                <tr>
-                    <td>Id</td>
-                    <td>Numele</td>
-                    <td>Prenumele</td>
-                    <td>Varsta</td>
-                    <td></td>
-                </tr>
-                <?php
-                         require_once 'models/DataBase.php';
-                         $obj = new DataBase();
-                         $users= $obj->getAllUser();
-                foreach($users as $id => $user):
-                ?>
-                <tr>
-                    <td><?= $id ?></td>
-                    <td><?= $user['fName'] ?></td>
-                    <td><?= $user['lName'] ?></td>
-                    <td><?= $user['age'] ?></td>
-                    <td>
-                        <form action="controllers/IndexController.php" method="get">
-                            <button name="id" value="<?= $id ?>">Delete</button>
-                        </form>
-                    </td>
-                </tr>
-
-                <?php endforeach; ?>
-
-            </table>
-        </div>
-    </div>
-
-    <div class="clearfix"></div>
-
-    <p>Lorem ipsum</p>
-    <div class="clearfix">
-        <div class="box">
-            Lorem Ipsum.
-        </div>
-        <div class="box">
-            Lorem Ipsum.
-        </div>
-        <div class="box">
-            Lorem Ipsum.
-        </div>
-    </div>
-</div>
-</body>
-
-<footer>
-    <br/><br/><br/>
-</footer>
-
-</html>
+switch ($_SERVER['REQUEST_METHOD']) {
+    case 'GET':
+        $users = UserModel::findAll();
+        $view_users = view("view/users.php", ["users"=>$users]);
+        $view_body = view("view/main.php", ["body"=>$view_users]);
+        echo view("view/layout.php", [
+            "title"=>"users_page",
+            "body"=>$view_body
+        ]);
+        break;
+    case 'POST':
+        switch ($_POST['action']){
+            case 'new_user':
+                $user = new UserModel([
+                    "fName"=>$_POST['fName'],
+                    "lName"=>$_POST['lName'],
+                    "age"=>$_POST['age'],
+                ]);
+                $save = $user->save();
+                if ($save){
+                    echo json_encode([
+                        'done'=> true,
+                        'data'=> [
+                            'id'=>$save,
+                            "fName"=>$user->fName,
+                            "lName"=>$user->lName,
+                            "age"=>$user->age,
+                        ]
+                    ]);
+                } else {
+                    echo json_encode([
+                        'done'=> false,
+                        'error'=>"something bad"
+                    ]);
+                }
+                break;
+            case 'delete_user':
+                $user = UserModel::findByPk($_POST['user_id']);
+                if($user){
+                    $user->delete();
+                    echo json_encode([
+                        'done'=> true,
+                        'data'=> []
+                    ]);
+                } else {
+                    echo json_encode([
+                        'done'=> false,
+                        'error'=>"something bad 1"
+                    ]);
+                }
+        }
+        break;
+}
+?>
