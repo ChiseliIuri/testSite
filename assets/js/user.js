5605e956a54055df40a21e7f034440c260bd6cbd
i.cujba <i.kuzhba@qiwi.com>
2021-09-01 17:05:43 +0300

    refactor user.js

--- assets/js/user.js
+++ assets/js/user.js
@@ -1,35 +1,40 @@
-function save_User() {
-    var finalObj = {};
-    $.each($('form#user_form').serializeArray(), function () {
-        finalObj[this.name] = this.value;
-    });
-
-    $.post('/testSite/',finalObj).done(function(result){
-        if (result.done){
-            $("#user_form").get(0).reset();
-            var user = result.data;
-            $("table#user_table>tbody").append(`<tr>
-                        <td>${user.id}</td>
-                        <td>${user.fName}</td>
-                        <td>${user.lName}</td>
-                        <td>${user.age}</td>
-                        <td>
-                             <button type="button" class="delete_user" data-id="${user.id}">Delete</button>
-                        </td>
-                    </tr>`)
-            toastr.success("Added with success");
-        } else {
-            console.error("ceva ne to");
-            toastr.error(result.error);
-        }
-    });
-}
-
-function toggle_form() {
-    $('#user_form').toggle();
-}
-
 $(document).ready(function(){
+    window.user_form = $('#user_form')
+    // toggle form
+    window.toggle_form = function() {
+        user_form.toggle();
+    }
+
+    // save user
+    window.save_User = function() {
+        var finalObj = {};
+        $.each(user_form.serializeArray(), function () {
+            finalObj[this.name] = this.value;
+        });
+
+        $.post('/testSite/',finalObj).done(function(result){
+            if (result.done){
+                user_form.get(0).reset();
+                var user = result.data;
+                $("table#user_table>tbody").append(`<tr>
+                    <td>${user.id}</td>
+                    <td>${user.fName}</td>
+                    <td>${user.lName}</td>
+                    <td>${user.age}</td>
+                    <td>
+                         <button type="button" class="delete_user" data-id="${user.id}">Delete</button>
+                    </td>
+                </tr>`);
+                user_form.hide();
+                toastr.success("Added with success");
+            } else {
+                console.error("ceva ne to");
+                toastr.error(result.error);
+            }
+        });
+    }
+
+    // delete user
     $("#user_table").on("click", ".delete_user", function(){
         var user_id = $(this).data("id");
         var button_obj = $(this);
