0e8b9e52fc12e2851a9940d28b0d6aa95bbd7665
i.cujba <i.kuzhba@qiwi.com>
2021-09-01 11:06:18 +0300

    move style.css in assets/css

--- view/layout.php
+++ view/layout.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?=$title?></title>
-    <link rel="stylesheet" type="text/css" href="assets/style.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css">
     <!--    <link href="assets/css/a" rel="stylesheet">-->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
